39d14f42240a21e6ff1cff191b3a77b44bab89c3
Caspar-Brantberger <3tomu24.branca@folkuniversitetet.nu>
2025-04-23 15:23:14 +0200

    Added component test

--- src/main/java/UserController.java
+++ src/main/java/UserController.java
@@ -1,3 +1,5 @@
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,9 +24,18 @@ public class UserController {
         return userService.findById(id);
     }
 
+    //@PostMapping
+    //public  User postUser(@RequestBody User user) {
+      //  return userService.createUser(user);
+    //}
     @PostMapping
-    public  User postUser(@RequestBody User user) {
-        return userService.createUser(user);
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+       try {
+           userService.createUser(user);
+           return ResponseEntity.status(HttpStatus.CREATED).body("User created");
+       } catch (Exception e) {
+           return ResponseEntity.status(HttpStatus.CONFLICT).body("User already exists");
+       }
     }
 
     @DeleteMapping
--- src/test/java/UserControllerTest.java
+++ src/test/java/UserControllerTest.java
@@ -1,5 +1,59 @@
-import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+
+
+import java.util.Optional;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
 
 class UserControllerTest {
 
-}
+    private UserRepository mockRepo;
+    private UserService userService;
+    private UserController userController;
+
+    @BeforeEach
+    void setUp() {
+        mockRepo = mock(UserRepository.class);
+        userService = new UserService(mockRepo);
+        userController = new UserController(userService);
+    }
+
+    @AfterEach
+    void tearDown() {
+        mockRepo = null;
+        userService = null;
+        userController = null;
+    }
+
+    //KomponentTest
+
+    @Test
+     public void testgetUser() {
+
+        User user = new User(1L, "John", "@Doe");
+        when(mockRepo.findById(1L)).thenReturn(Optional.of(user));
+
+        Optional<User> userOptional = userController.getUserById(1L);
+
+        assertEquals("John", userOptional.get().getName());
+        assertEquals("@Doe", userOptional.get().getEmail());
+
+    }
+
+
+    }
+
+
+
+
